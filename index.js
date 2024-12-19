83ccbe1ad40f314f3f5a7265b9393145a0b14ee1
Amauri Junior <mendesamauri177@gmail.com>
2024-12-18 23:28:00 -0300

    Exercício dia da semana (Switch Case JS)

---
+++ index.js
@@ -0,0 +1,47 @@
+let diaSemana = Number(prompt("Digite um número de 1 a 7 para saber o dia da semana: "))
+
+switch(diaSemana){
+    case 1:
+        console.log('O dia 1 da semana é DOMINGO')
+        break;
+    case 2:
+        console.log('O dia 2 da semana é SEGUNDA-FEIRA')
+        break;
+    case 3:
+        console.log('O dia 3 da semana é TERÇA-FEIRA')
+        break;
+    case 4:
+        console.log('O dia 4 da semana é QUARTA-FEIRA')
+        break;
+    case 5:
+        console.log('O dia 5 da semana é QUINTA-FEIRA')
+        break;
+    case 6:
+        console.log('O dia 6 da semana é SEXTA-FEIRA')
+        break;
+    case 7:
+            console.log('O dia 7 da semana é SABADO')
+            break;
+    default:
+            console.log('Número inválido')
+            break; 
+}
+
+
+
+
+
+
+
+
+
+//Verificando o Nome do Dia da Semana
+//Escreva um programa que recebe um número de 1 a 7 e imprime o nome do dia da semana correspondente:
+
+//1: Domingo
+//2: Segunda-feira
+//3: Terça-feira
+//4: Quarta-feira
+//5: Quinta-feira
+//6: Sexta-feira
+//7: Sábado
