fb7649268f1627e16b0586be50736577e038919b
Amauri Junior <mendesamauri177@gmail.com>
2024-12-21 00:22:07 -0300

    Verificação de idade em Js (If/else)

---
+++ Exercícios de If/Else em JS/verificarMaiorIdade.js
@@ -0,0 +1,24 @@
+let idade = Number(prompt('Informe sua idade: '))
+
+if(idade >= 18){
+    console.log('Você tem ', idade , 'anos e é Maior de idade ! ')
+}else{
+    console.log('Você tem ', idade , 'anos e não atingiu a Maior de idade ! ')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Verificar Maioridade
+//Receba a idade de uma pessoa e informe se ela é maior de idade (18 anos ou mais) ou menor de idade.
