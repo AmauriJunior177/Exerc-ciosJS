ed0d983de82c8937e039fe66ac6cbff5280f99b3
Amauri Junior <mendesamauri177@gmail.com>
2024-12-21 00:11:27 -0300

    Exercício dia da semana Js (FOR)

---
+++ Exercícios de FOR em JS/diadasemana.js
@@ -0,0 +1,31 @@
+let num = Number(prompt('Digite um número de 1 a 7 para saber o dia da semana: '))
+
+switch(num){
+    case 1:
+        console.log('Na semana o dia 1 é o DOMINGO!')
+        break;
+    case 2:
+        console.log('Na semana o dia 2 é o SEGUNDA!')
+        break;
+    case 3:
+        console.log('Na semana o dia 3 é o TERÇA!')
+        break;
+    case 4:
+        console.log('Na semana o dia 4 é o QUARTA!')
+        break;
+    case 5:
+        console.log('Na semana o dia 5 é o QUINTA!')
+        break;
+    case 6:
+        console.log('Na semana o dia 6 é o SEXTA!')
+        break;
+    default:
+        console.log('Na semana o dia 7 é o SÁBADO!')
+        break;       
+}
+
+
+
+
+//Dia da Semana
+//Crie um programa que receba um número de 1 a 7 e exiba o dia da semana correspondente (1 = Domingo, 2 = Segunda, etc.).
